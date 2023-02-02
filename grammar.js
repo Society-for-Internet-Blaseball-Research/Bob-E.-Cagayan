e7d85a0444b4448341524aa6fbf44ef05d837773
Kawa <kawaii.dragoness@gmail.com>
2023-02-02 15:05:13 -0500

    Create grammar.js

---
+++ grammar.js
@@ -0,0 +1,107 @@
+{
+"origin":[
+"#player# hops on the Grind Rail toward third base. \nThey do a #trick# (#score#)! \n#second-attempt#", 
+"#player# hops on the Grind Rail toward third base. \nThey do a #trick# (#score#)! \n#second-attempt#", 
+"#player# hops on the Grind Rail toward third base. \nThey do a #trick# (#score#)! \n#second-attempt#", 
+"#birds#",
+"#peanuts#", 
+"Hype built in the Escher Complex!", 
+"SALMON CANNONS FIRE \n CONSUMER EXPELLED",
+"#onomatopoeia#!\n#playerCaps# #wrestleMoved# A CONSUMER!"
+],
+
+"second-attempt":[
+"They land a #trick# (#score#)! \nSafe!", 
+"They land a #trick# (#score#)! \nSafe!", 
+"They land a #trick# (#score#)! \nSafe!", 
+" ... but lose their balance and bail! Out!", 
+"They're tagged out doing a #trick# (#score#)!"
+],
+
+"birds":["Where did these birds come from?","What are we gonna do with all these birds?","This is too many birds.","These birds hate Blaseball!","There's just too many birds!","The birds continue to stare.","The Birds circle ... but they don't find what they're looking for.","The birds are very loud!","The birds are paralyzed! They can't move!","The birds are mad at you. You specifically. You know who you are.","The birds are after the children...","Several birds are pecking...","I hardly think a few birds are going to bring about the end of the world.","Have you ever seen this many birds?","Don't feed the birds","Do these birds have souls?","BIRD NOISES","#bird-score# Birds"],
+
+"bird-score":["#leading##digit##digit##digit#", "#leading##digit##digit#","#leading##digit##digit#","#leading##digit##digit#", "#leading##digit##digit#", "#leading##digit##digit#", "#leading##digit#", "#leading#"],
+
+"peanuts":["A Big Peanut crashes into the field. It lands harmlessly on the field.","A desolate peanutty wind blows.","A solitary peanut rolls onto the field. Nobody cares.","Peanut fragments rustle on the infield.","The sour smell of rancid peanuts on the wind"],
+
+"player": ["Turing Capybara", "Datum Criminale", "None Binary", "Batista Oatmilk", "Major Arcana", "Bob E. Cagayan", "Strike, Looking", "Beans Reblase", "Zero Index", "Jason Datablase", "PolkaDot Matrix", "Espresso Machine", "Nebula Holstein", "Molly Tapedeck", "Mason M. McMason", "Boxplot Jones", "Algebra Goldberg", "Number Cruncher", "Hopper Lovelace", "Hex Portential", "Games Pergame"],
+
+"playerCaps": [
+"TURING CAPYBARA",
+ "DATUM CRIMINALE",
+ "NONE BINARY",
+ "BATISTA OATMILK",
+ "MAJOR ARCANA",
+ "BOB E. CAGAYAN",
+ "STRIKE, LOOKING",
+ "BEANS REBLASE",
+ "ZERO INDEX",
+ "JASON DATABLASE",
+ "POLKADOT MATRIX",
+ "ESPRESSO MACHINE",
+ "NEBULA HOLSTEIN",
+ "MOLLY TAPEDECK",
+ "MASON M. MCMASON",
+ "BOXPLOT JONES",
+ "ALGEBRA GOLDBERG",
+ "NUMBER CRUNCHER",
+ "HOPPER LOVELACE",
+ "HEX PORTENTIAL",
+ "GAMES PERGAME"
+],
+
+"onomatopoeia": [
+"ARGH",
+"BLAM",
+"BOOM",
+"CRACK",
+"CRRRACK",
+"CRUNCH",
+"KABLAM",
+"KABOOM",
+"KAPOW",
+"KA-THOOM",
+"KERPLUNK",
+"POW",
+"SHABOOM",
+"SLAM",
+"SNAP",
+"THUD",
+"THWACK",
+"WHAM",
+"WHAMMO"
+],
+
+"wrestleMoved":[
+"AVOCADO TOASTED",
+"BATTERING RAMMED",
+"BICYCLE KICKED",
+"CHOPPED",
+"CLOTHESLINED",
+"CRANE KICKED",
+"DROPKICKED",
+"ELBOWED",
+"JACKHAMMERED",
+"LEG DROPPED",
+"NOSE SLAMMED",
+"PILEDRIVERED",
+"POWERBOMBED",
+"SLAMMED",
+"SLAPPED",
+"SPEARED",
+"SUPLEXED",
+"THUNDERCLAPPED",
+"WINDMILL SLAMMED"
+],
+
+
+"trick": ["Upshell","1080","Llama Drama","Flipflip","Dumpster Flip","Nothing Burger","Grape Flip","Triple Ollie","Soul Crusher","Flip Flop","777","Loop-de-Loop","Sky Wink","270","Heelflip","Flop Flip","90210","Over Over","Handstand","540","Kickblip","Fakie Beta","Please Imagine Music","No Mayo","So Below","Flip Phone","Hot Dog Stand","Kickflip","Backside 180 Kickflip","Log Off","Airwalk","Oh No","Finger Flip","Sunflip","Moon Haunter","T Pose","Umpdog","Static Hummingbird","Tech Debt","Underflip","Toejam and Swirl","Jumping Jack","Spark Rail","Levitate","Newt","Flakie","Big Spinny","Cliffhanger","Wiggleflip","Clutch","Bloodflip","Rocket Air","Frontside 180 Kickflip","Seatbelt Grab","Slip and Slide","Beanslide","Minesweeper","Heel Turn","Hot Fix","Nosetoucher","Blahja Blast","Data Crime","Toe Touch","Groundskeeper","McTwist","Frontflip","Hot Lasagna","Sharkparty","Wheelie","50-50","Silver Surf","Pop Shuvit","Electric Slide","Dinner Spin","Ska Walker","Slouch Crouch","Line Wobbler","Wafflemaker","Ticker Scroll","Slide Whistle","Ollie Impossible","Pancake Flip","50-50-50","Tippy Tappy","Triple Grind","Standstand","Fourleg","Shell Slide","Whammy Bar","Doomscroller","Blue Check","Trail Mixer","99","Flamingo","Moonwalk Five-O","Nose to Tail","The Fourth Slide","Hashtag","Dead Shred","Bean Grind","Ollie Oxenfree","Bonerattler","Grand Unslide","As Above","Big Big Spinny","Blood Dragon","Slimpossible","Cha Cha Slide","Cat's Cradle","Fan Service","Metaltoucher","Immaterial Plain","Dollhouse","Darkslide","Spacewalk","Powerline","Pickle Slicer","Under Under","Ollie"],
+
+"score":["#thousands##digit##digit##digit#","#thousands##digit##digit##digit#", "#leading##digit##digit#","#leading##digit##digit#","#leading##digit##digit#", "#leading##digit##digit#", "#leading##digit##digit#", "#leading##digit#"],
+
+"thousands":["1","2","3","4","5"],
+
+"leading": ["1","2","3","4","5","6","7","8","9"],
+
+"digit":["0","1","2","3","4","5","6","7","8","9"]
+}
